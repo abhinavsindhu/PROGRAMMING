c8f2c7f45803aff28fc377cd8b8a3fb85953dfd7
abhinavsindhu <codenamex97@gmail.com>
2017-12-18 13:21:21 +0530

    add new primes
    add new primes

--- MAGICALWORD/main.c
+++ MAGICALWORD/main.c
@@ -4,31 +4,33 @@
 int main()
 {
     int t,i,n,a,b,d1,d2;
-    int A[]={67,71,73,79,83,97,101,103,107,113};
+    int A[]={67,71,73,79,83,89,97,101,103,107,109,113};
     char str[500];
     scanf("%d",&t);
 
     while(t--)
     {
         scanf("%s",str);
-        for(i=0;str[i]!='\0';i++)
+        while(str[i])
+        {
+      for(n=0;n<10;n++)
       {     if(str[i]<67)
            str[i]=A[0];
            else if(str[i]>113)
             str[i]=A[9];
            else
             {
-            d1=str[i]-A[i];
-            d2=A[i+1]-str[i];
+            d1=str[i]-A[n];
+            d2=A[n+1]-str[i];
             if(d1<=d2)
-                str[i]=A[i];
+                str[i]=A[n];
             else
-                str[i]=A[i+1];
+                str[i]=A[n+1];
 
             }
 
 
-
+      }
       }
     }
       printf("%s",str);
