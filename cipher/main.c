4021bb2193e6f29d4232541cec59f602e3db3e5b
abhinavsindhu <codenamex97@gmail.com>
2017-12-21 13:42:52 +0530

    cipher

--- cipher/main.c
+++ cipher/main.c
@@ -3,14 +3,19 @@
 
 int main()
 {
-    char ch[10];
-    int n,i,n1;
+    char ch[100 ];
+    int n,i,n1,nc;
     scanf("%s",ch);
     scanf("%d",&n);
+    nc=n;
     for(i=0;ch[i]!='\0';i++)
     {
         if(ch[i]>=65&&ch[i]<=90)
         {
+            while(n>=26)
+                {
+                    n=n%26;
+                }
             if(ch[i]+n>90)
             {
                 n1=(ch[i]+n)-90;
@@ -18,9 +23,14 @@ int main()
             }
             else
                 ch[i]=ch[i]+n;
+                n=nc;
         }
         else if(ch[i]>=97&&ch[i]<=122)
         {
+            while(n>=26)
+                {
+                    n=n%26;
+                }
             if(ch[i]+n>122)
             {
                 n1=(ch[i]+n)-122;
@@ -28,9 +38,14 @@ int main()
             }
             else
                 ch[i]=ch[i]+n;
+                n=nc;
         }
         else if(ch[i]>=48&&ch[i]<=57)
         {
+            while(n>=10)
+                {
+                    n=n%10;
+                }
             if(ch[i]+n>57)
             {
             n1=(ch[i]+n)-57;
@@ -38,6 +53,7 @@ int main()
             }
         else
             ch[i]=ch[i]+n;
+            n=nc;
         }
     }
     printf("%s",ch);
