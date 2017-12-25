af41392798c2adf73267fbf1a91948eee0e03fb9
abhinavsindhu <codenamex97@gmail.com>
2017-12-25 23:50:40 +0530

    NEW
    NEW

--- array/main.c
+++ array/main.c
@@ -3,17 +3,20 @@
 int annagram(char[],char[]);
 int main()
 {
-   int flag;
-   char str1[100],str2[100];
+   int flag,t;
+   char str1[100000],str2[100000];
+   scanf("%d",&t);
+   while(t--)
+   {
    scanf("%s",str1);
    scanf("%s",str2);
    flag=annagram(str1,str2);
 
    if(flag==1)
-   printf("%s and %s are annagram",str1,str2);
+   printf("YES\n");
    else
-    printf("%s and %s are annagram",str1,str2);
-
+    printf("NO\n");
+   }
     return 0;
 }
 int annagram(char str1[],char str2[])
@@ -22,13 +25,13 @@ int annagram(char str1[],char str2[])
     int c=0;
     while(str1[c]!='\0')
     {
-        A[str1[c]-'a'];
+        A[str1[c]-'a']++;
         c++;
     }
     c=0;
     while(str2[c]!='\0')
     {
-        B[str2[c]-'a'];
+        B[str2[c]-'a']++;
         c++;
     }
     for(c=0;c<26;c++)
@@ -36,6 +39,6 @@ int annagram(char str1[],char str2[])
         if(A[c]!=B[c])
             return 0;
     }
-    return 0;
+    return 1;
 
 }
